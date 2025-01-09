6d2ba082431b80f16b5d6e066634075197445817
ROHIT YADAV <rohitkyadav2312@gmail.com>
2025-01-09 11:59:10 +0530

    Rust_Projects

--- read_csv/src/main.rs
+++ read_csv/src/main.rs
@@ -13,9 +13,15 @@ fn main()->Result<(), Box<dyn Error>> {
         // println!("Carr name {} and Model Year is 19{}",record[0].to_string(),record[7].to_string());
 
        
-        if record[7].to_string()<"71".to_string()
+        // if record[7].to_string()<"71".to_string()
+        // {
+        //     println!("Carr name {} and Model Year is 19{}",record[0].to_string(),record[7].to_string());
+        //     count=count+1;
+        // }
+
+        if record[8].to_string() == "usa"
         {
-            println!("Carr name {} and Model Year is 19{}",record[0].to_string(),record[7].to_string());
+            println!("Car From USA {}",record[0].to_string());
             count=count+1;
         }
     }
